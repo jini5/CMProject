2c21f7c4db09c5f080184825a7cd324faa102721
jini5 <lim789789@gmail.com>
2023-04-25 22:24:35 +0900

    Initial commit

---
+++ src/main/java/com/example/cmproject/CmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cmproject/CmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
