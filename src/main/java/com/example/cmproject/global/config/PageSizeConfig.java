b0e290be975ae1570e262e2973a7f7aed222bff1
jini5 <lim789789@gmail.com>
2023-05-12 01:17:39 +0900

    feat: PageSizeConfig

---
+++ src/main/java/com/example/cmproject/global/config/PageSizeConfig.java
@@ -0,0 +1,7 @@
+package com.example.cmproject.global.config;
+
+public class PageSizeConfig {
+
+    public static final int User_List_Size = 20;
+    public static final int POST_LIST_SIZE = 20;
+}
