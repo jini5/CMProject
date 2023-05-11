d045b4b755308628109974a0cb41590e044335e9
jini5 <lim789789@gmail.com>
2023-05-12 00:59:53 +0900

    feat: PostDTO - 게시글 생성, 수정, 삭제, 조회, 상세조회

---
+++ src/main/java/com/example/cmproject/dto/PostDTO.java
@@ -0,0 +1,181 @@
+package com.example.cmproject.dto;
+
+import com.example.cmproject.entity.Category;
+import com.example.cmproject.entity.Post;
+import com.example.cmproject.entity.Role;
+import com.example.cmproject.entity.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class PostDTO {
+
+    @Getter
+    @Setter
+    @Builder
+    @ApiModel(value = "게시글 생성")
+    public static class CreatePostReqDTO {
+
+        @NotNull
+        private Long userId;
+
+        @ApiParam(value = "카테고리 ID", required = true)
+        @NotNull(message = "카테고리를 선택해 주세요.")
+        private Long categoryId;
+
+        @ApiParam(value = "게시글 제목", required = true)
+        @NotBlank(message = "제목을 입력해 주세요.")
+        private String title;
+
+        @ApiParam(value = "게시글 내용", required = true)
+        @NotBlank(message = "내용을 입력해 주세요.")
+        private String content;
+
+        public Post toEntity(User user, Category category) {
+            return Post.builder()
+                    .user(user)
+                    .category(category)
+                    .title(title)
+                    .content(content).build();
+        }
+
+    }
+
+    @Getter
+    @Builder
+    @ApiModel(value = "게시글 수정")
+    public static class UpdateReqDTO {
+
+        @ApiParam(value = "카테고리 ID", required = true)
+        @NotNull(message = "카테고리를 선택해 주세요.")
+        private Long categoryId;
+
+        @ApiParam(value = "글 제목", required = true)
+        @NotBlank(message = "제목을 입력해 주세요.")
+        private String title;
+
+        @ApiParam(value = "글 내용", required = true)
+        @NotBlank(message = "내용을 입력해 주세요.")
+        private String content;
+
+        @ApiModelProperty(value = "게시글 수정일자")
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        private LocalDate updatedDate;
+
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @ApiModel(value = "게시글 삭제")
+    public class DeletePostReqDTO {
+        @NotNull
+        @ApiModelProperty(value = "게시글 아이디")
+        private Long postId;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @ApiModel(value = "게시글 상세 조회")
+    public static class PostDetailResDTO {
+
+        @ApiModelProperty(notes = "게시글 ID")
+        private Long postId;
+
+        @ApiModelProperty(notes = "작성자 ID")
+        private Long userId;
+
+        @ApiModelProperty(notes = "카테고리 ID")
+        private Long categoryId;
+
+        @ApiModelProperty(notes = "작성자 닉네임")
+        private String userNickname;
+
+        @ApiModelProperty(notes = "카테고리 이름")
+        private String categoryName;
+
+        @ApiModelProperty(notes = "게시글 제목")
+        private String title;
+
+        @ApiModelProperty(notes = "게시글 내용")
+        private String content;
+
+        @ApiModelProperty(notes = "로그인 한 유저의 좋아요 여부")
+        private boolean likeStatus;
+
+        @ApiModelProperty(notes = "댓글 리스트")
+        private List<CommentDTO.CommentResDTO> commentList;
+
+        @ApiModelProperty(value = "게시글 생성일자")
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        private LocalDate createdDate;
+
+        @ApiModelProperty(value = "게시글 수정일자")
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        private LocalDate updatedDate;
+
+        @ApiModelProperty(notes = "카테고리 Role")
+        private Role categoryRole;
+
+        @Builder
+        public PostDetailResDTO(Post post, boolean isLiked) {
+            this.postId = post.getPostId();
+            this.userId = post.getUser().getUserId();
+            this.categoryId = post.getCategory().getCategoryId();
+            this.userNickname = post.getUser().getNickName();
+            this.categoryName = post.getCategory().getName();
+            this.title = post.getTitle();
+            this.content = post.getContent();
+            this.likeStatus = isLiked;
+            this.commentList = getCommentList();
+            this.createdDate = getCreatedDate();
+            this.updatedDate = getUpdatedDate();
+            this.categoryRole = post.getCategory().getRole();
+        }
+
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PostResDTO {
+
+        @ApiModelProperty(notes = "게시물 ID")
+        private Long postId;
+
+        @ApiModelProperty(notes = "제목")
+        private String title;
+
+        @ApiModelProperty(notes = "작성자 닉네임")
+        private String nickname;
+
+        @ApiModelProperty(notes = "좋아요 수")
+        private int likeCount;
+
+        @ApiModelProperty(notes = "작성일자")
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        private LocalDateTime createdDate;
+
+        @ApiModelProperty(value = "게시글 마지막 수정일자")
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        private LocalDateTime updatedDate;
+
+        public PostResDTO(Post post, User user) {
+            this.postId = post.getPostId();
+            this.title = post.getTitle();
+            this.nickname = user.getNickName();
+            this.likeCount = post.getPostLikes().size();
+            this.createdDate = post.getCreatedDate();
+            this.updatedDate = post.getUpdatedDate();
+        }
+
+    }
+}
