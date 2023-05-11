c1bff0ed1b1826c454210016df62e1dd4dbfa743
jini5 <lim789789@gmail.com>
2023-05-12 01:05:43 +0900

    feat: CommentLikeRepository, CommentRepository

---
+++ src/main/java/com/example/cmproject/repository/CommentLikeRepository.java
@@ -0,0 +1,11 @@
+package com.example.cmproject.repository;
+
+import com.example.cmproject.entity.Comment;
+import com.example.cmproject.entity.CommentLike;
+import com.example.cmproject.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+
+    CommentLike findByUserAndComment(User user, Comment comment);
+}
