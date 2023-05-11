7ade34dfb04665daff525f4a21123356a8991c28
jini5 <lim789789@gmail.com>
2023-05-12 01:16:38 +0900

    feat: Controller

---
+++ src/main/java/com/example/cmproject/controller/CommentController.java
@@ -0,0 +1,66 @@
+package com.example.cmproject.controller;
+
+
+import com.example.cmproject.dto.CommentDTO;
+import com.example.cmproject.dto.UserDTO;
+import com.example.cmproject.service.CommentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@Api(tags = {"댓글 서비스"})
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@RequestMapping("/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+
+    @ApiOperation(value = "댓글 작성", notes = "댓글 작성.\n\n" + "code: 200 조회 성공, 400 잘못된 postId 요청")
+    @GetMapping("/{postId}")
+    public ResponseEntity<?> createComment(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO, CommentDTO.CreateCommentReqDTO createCommentReqDTO,@PathVariable Long postId) {
+        return commentService.createComment(userAccessDTO,createCommentReqDTO,postId);
+    }
+
+    @ApiOperation(value = "대댓글 작성", notes = "대댓글 작성.\n\n" + "code: 200 조회 성공, 400 잘못된 postId 혹은 commentId 요청")
+    @GetMapping("/{postId}/{commentId}")
+    public ResponseEntity<?> createReComment(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO, CommentDTO.ReCommentReqDTO reCommentReqDTO,@PathVariable Long commentId,@PathVariable Long postId) {
+        return commentService.createReComment(userAccessDTO,reCommentReqDTO,commentId,postId);
+    }
+
+    @ApiOperation(value = "댓글 삭제", notes = "댓글, 대댓글 삭제.\n\n" + "code: 200 조회 성공, 400 잘못된 commentId 요청")
+    @GetMapping("/{commentId}")
+    public ResponseEntity<?> deleteComment(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO,@PathVariable Long commentId) {
+        return commentService.deleteComment(userAccessDTO,commentId);
+    }
+
+    @ApiOperation(value = "댓글 수정", notes = "댓글, 대댓글 수정.\n\n" + "code: 200 조회 성공, 400 잘못된 commentId 요청")
+    @GetMapping("/{commentId}/update")
+    public ResponseEntity<?> updateComment(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO,CommentDTO.UpdateCommentReqDTO updateCommentReqDTO,@PathVariable Long commentId) {
+        return commentService.updateComment(userAccessDTO, updateCommentReqDTO,commentId);
+    }
+
+    @ApiOperation(value = "댓글 좋아요", notes = "댓글에 대해 좋아요 버튼을 누른다.\n\n" + "code: 200 조회 성공, 400 잘못된 commentId 요청")
+    @GetMapping("/{commentId}/like")
+    public ResponseEntity<?> LikeComment(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO, @PathVariable Long commentId) {
+        return commentService.likeComment(userAccessDTO, commentId);
+    }
+
+    @ApiOperation(value = "댓글  싫어요", notes = "댓글에 대해 싫어요 버튼을 누른다.\n\n" + "code: 200 조회 성공, 400 잘못된 commenttId 요청")
+    @GetMapping("/{commentId}/dislike")
+    public ResponseEntity<?> disLikeComment(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO, @PathVariable Long commentId) {
+        return commentService.disLikeComment(userAccessDTO, commentId);
+    }
+
+    @ApiOperation(value = "댓글  좋아요/싫어요 삭제", notes = "댓글에 대해 싫어요 버튼을 누른다.\n\n" + "code: 200 조회 성공, 400 잘못된 commentId 요청")
+    @GetMapping("/{commentId}/cancel")
+    public ResponseEntity<?> cancelCommentLike(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO, @PathVariable Long commentId) {
+        return commentService.cancelCommentLike(userAccessDTO, commentId);
+    }
+
+}
---
+++ src/main/java/com/example/cmproject/controller/TestController.java
@@ -0,0 +1,56 @@
+package com.example.cmproject.controller;
+
+import com.example.cmproject.dto.UserDTO;
+import com.example.cmproject.service.AdminService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@Api(tags = {"테스트 서비스"}, description = "유저, 관리자 관련 테스트 (배포시 삭제)")
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@RequestMapping("/test")
+public class TestController {
+
+    private final AdminService adminService;
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER')")
+    /**유저 권한만 허용 **/
+    @ApiOperation(value = "User 권한 확인", notes = "ROLE_USER 인 경우만 작동")
+    public ResponseEntity<?> checkUser(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO) {
+        return new ResponseEntity<>(userAccessDTO.getEmail(), HttpStatus.OK);
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @ApiOperation(value = "ADMIN 권한 확인", notes = "ROLE_ADMIN 인 경우만 작동")
+    public ResponseEntity<?> checkAdmin(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO) {
+        return new ResponseEntity<>(userAccessDTO.getEmail(), HttpStatus.OK);
+    }
+
+    @GetMapping("/role")
+    @ApiOperation(value = "권한 확인", notes = "모든 권한에서 작동")
+    public ResponseEntity<?> checkRole(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO) {
+        return new ResponseEntity<>(userAccessDTO.getRole(), HttpStatus.OK);
+    }
+
+    @GetMapping("/selfAdmin")
+    @ApiOperation(value = "개발 테스트용 관리자 승격", notes = "현재 사용자 관리자로 승격 (main 올릴땐 삭제)")
+    public ResponseEntity<?> selfAdmin(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO) {
+        return adminService.setUserToAdmin(userAccessDTO.getEmail());
+    }
+
+    @GetMapping("/UpdatePassword")
+    @ApiOperation(value = "개발 테스트용 비밀번호 변경", notes = "비밀번호 변경")
+    public ResponseEntity<?> selfAdmin(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO, @RequestBody UserDTO.UpdatePWReqDTO updatePWReqDTO) {
+        return adminService.updatePw(userAccessDTO,updatePWReqDTO);
+    }
+
+}
